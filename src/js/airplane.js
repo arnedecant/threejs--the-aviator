fa6e8e2bb3d9b7884c4863fd5e9070cd868cc589
Arne Decant <hello@arnedecant.be>
2018-12-11 22:26:18 +0100

    added wheels to the airplane

--- src/js/airplane.js
+++ src/js/airplane.js
@@ -22,6 +22,8 @@ class Airplane {
 		this.createTail()
 		this.createWing()
 		this.createPropeller()
+		this.createWheelsProtection()
+		this.createWheels()
 
 		this.createPilot()
 
@@ -32,7 +34,7 @@ class Airplane {
 
 	createCabin() {
 
-		let geometry = new THREE.BoxGeometry(60, 50, 50, 1, 1, 1)
+		let geometry = new THREE.BoxGeometry(80, 50, 50, 1, 1, 1)
 		let material = new THREE.MeshPhongMaterial({
 			color: this.colors.red,
 			flatShading: true
@@ -85,7 +87,7 @@ class Airplane {
 
 		let tail = new THREE.Mesh(geometry, material)
 
-		tail.position.set(-35, 25, 0)
+		tail.position.set(-40, 20, 0)
 
 		tail.castShadow = true
 		tail.receiveShadow = true
@@ -159,6 +161,89 @@ class Airplane {
 
 		// add the pilot to the mesh
 		this.mesh.add(this.pilot.mesh)
+
+	}
+
+	createWheelsProtection() {
+
+		let geometry = new THREE.BoxGeometry(30, 15, 10, 1, 1, 1)
+		let material = new THREE.MeshPhongMaterial({
+			color: this.colors.red,
+			flatShading: true
+		})
+
+		let protectionR = new THREE.Mesh(geometry, material)
+		protectionR.position.set(25, -20, 25)
+
+		let protectionL = protectionR.clone()
+		protectionL.position.z = 0 - protectionR.position.z
+
+		this.mesh.add(protectionR)
+		this.mesh.add(protectionL)
+
+	}
+
+	createWheels() {
+
+		// create new group
+		let wheels = new THREE.Object3D()
+
+		// create a tire
+		let tireGeometry = new THREE.CylinderGeometry(12, 12, 6, 12)
+		let tireMaterial = new THREE.MeshPhongMaterial({
+			color: this.colors.black,
+			flatShading: true
+		})
+
+		let tireR = new THREE.Mesh(tireGeometry, tireMaterial)
+		tireR.position.set(25, -28, 25)
+		tireR.rotation.x = Math.PI / 2
+
+		// create a tire axis
+		let axisGeometry = new THREE.CylinderGeometry(4, 4, 7, 12)
+		let axisMaterial = new THREE.MeshPhongMaterial({
+			color: this.colors.brown,
+			flatShading: true
+		})
+
+		let axisR = new THREE.Mesh(axisGeometry, axisMaterial)
+
+		// add axis to tire
+		tireR.add(axisR)
+
+		// add tire to group
+		wheels.add(tireR)
+
+		// clone a tire: left
+		let tireL = tireR.clone()
+		tireL.position.z = 0 - tireR.position.z
+		wheels.add(tireL)
+
+		// clone a tire: back
+		let tireB = tireR.clone()
+		tireB.scale.set(0.5, 0.5, 0.5)
+		tireB.position.set(-35, -5, 0)
+		wheels.add(tireB)
+
+		// add back suspension
+		let suspensionGeometry = new THREE.BoxGeometry(4, 20, 4)
+		let suspensionMaterial = new THREE.MeshPhongMaterial({
+			color: this.colors.red, 
+			flatShading: true
+		})
+
+		suspensionGeometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,10,0))
+
+		let suspension = new THREE.Mesh(suspensionGeometry, suspensionMaterial)
+
+		suspension.position.set(-35,-5,0);
+		suspension.rotation.z = -.3;
+
+		// add suspension to group
+		wheels.add(suspension);
+
+		// add group to parent mesh
+		this.mesh.add(wheels)
 
 	}
 
